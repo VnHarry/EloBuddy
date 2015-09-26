8f04c0ce3503ec7865fc802a14fd3ee105175ca7
VnHarry <thanhtu162000@hotmail.com>
2015-09-26 13:59:49 +0700

    Update for Draven

--- Graves/Internal/Variables.cs
+++ Graves/Internal/Variables.cs
@@ -9,9 +9,10 @@ namespace VnHarry_AIO.Internal
         public static Menu Config;
         public static Menu Activator;
         public static Menu UniversalGankAlerter;
+        public static bool NoneMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.None;
         public static bool ComboMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo;
         public static bool HarassMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Harass;
-        public static bool LaneClearMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LaneClear;
+        public static bool LaneClearMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LaneClear || Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.JungleClear;
         public static bool LastHitMode = Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LastHit;
     }
 }
