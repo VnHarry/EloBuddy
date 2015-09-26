8f45263058458eee8cd59105c23bb35fe88afaaf
VnHarry <thanhtu162000@hotmail.com>
2015-09-26 20:11:34 +0700

    Update 1.0.0.3

--- Twisted Fate/StateHandler.cs
+++ Twisted Fate/StateHandler.cs
@@ -29,15 +29,15 @@ namespace VnHarry_Twisted_Fate
         {
             var target = TargetSelector2.GetTarget(GetDynamicRange() + 100, DamageType.Magical);
             if (target == null) return;
-            CardSelector.StartSelecting(Cards.Blue);
-            Program.Q.Cast(target);
+            //CardSelector.StartSelecting(Cards.Blue);
+            //Program.Q.Cast(target);
         }
 
         public static void LastHit()
         {
             var minion = ObjectManager.Get<Obj_AI_Minion>().FirstOrDefault(a => a.IsEnemy && a.Health <= QDamage(a));
             if (minion == null) return;
-            CardSelector.StartSelecting(Cards.Blue);
+            //CardSelector.StartSelecting(Cards.Blue);
         }
 
         public static void WaveClear()
@@ -48,27 +48,21 @@ namespace VnHarry_Twisted_Fate
             var useQ = Program.LaneClearMenu["drawings.q"].Cast<CheckBox>().CurrentValue;
             var useW = Program.LaneClearMenu["drawings.w"].Cast<CheckBox>().CurrentValue;
 
-            if (useW)
+            if (Program._Player.ManaPercent >= Program.LaneClearMenu["laneclear.mana"].Cast<Slider>().CurrentValue)
             {
-                if (Program._Player.ManaPercent >= Program.LaneClearMenu["laneclear.mana"].Cast<Slider>().CurrentValue)
+                if (useW) CardSelector.StartSelecting(Cards.Red);
+                foreach (Obj_AI_Minion minion in allMinionsQ)
                 {
-                    CardSelector.StartSelecting(Cards.Red);
-                    foreach (Obj_AI_Minion minion in allMinionsQ)
+                    if ((!Player.Instance.IsInAutoAttackRange(minion) || (!Orbwalker.CanAutoAttack && Orbwalker.LastTarget.NetworkId != minion.NetworkId)) && (minion.Health < 0.8 * QDamage(minion)))
                     {
-                        if ((!Player.Instance.IsInAutoAttackRange(minion) || (!Orbwalker.CanAutoAttack && Orbwalker.LastTarget.NetworkId != minion.NetworkId)) && (minion.Health < 0.8 * QDamage(minion)))
-                        {
-                            if (useQ)
-                            {
-                                Program.Q.Cast(minion);
-                            }
-                            break;
-                        }
+                        if (useQ) Program.Q.Cast(minion);
+                        break;
                     }
                 }
-                else
-                {
-                    CardSelector.StartSelecting(Cards.Blue);
-                }
+            }
+            else
+            {
+                if (useW) CardSelector.StartSelecting(Cards.Blue);
             }
         }
 
