4ca08d210718499b6a505c555c4015189ffe4c52
VnHarry <thanhtu162000@hotmail.com>
2015-09-26 22:29:19 +0700

    Diana 1.0.0.1

---
+++ Diana/Program.cs
@@ -0,0 +1,213 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using System;
+using System.Linq;
+
+namespace VnHarry_Diana
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        public static Spell.Skillshot Q;
+        public static Spell.Active W;
+        public static Spell.Active E;
+        public static Spell.Targeted R;
+
+        public static SpellSlot ignite;
+        public static bool gotIgnite = false;
+
+        public static AIHeroClient _Player { get { return ObjectManager.Player; } }
+        public static int Mana { get { return (int)_Player.Mana; } }
+        public static Menu DianaMenu, ComboMenu, HarassMenu, LaneClearMenu, DrawingsMenu;
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            //if (ObjectManager.Player.BaseSkinName != "TwistedFate") return;
+
+            TargetSelector2.init();
+            Bootstrap.Init(null);
+
+            Q = new Spell.Skillshot(SpellSlot.Q, 895, SkillShotType.Circular, (int)0.25f, (int)1400f, (int)195f);
+            W = new Spell.Active(SpellSlot.W, 240);
+            E = new Spell.Active(SpellSlot.E, 350);
+            R = new Spell.Targeted(SpellSlot.R, 825);
+            try
+            {
+                ignite = Player.Spells.FirstOrDefault(o => o.SData.Name == "summonerdot").Slot;
+                gotIgnite = true;
+            }
+            catch { }
+            TargetSelector.ActiveMode = TargetSelectorMode.LeastHealth;
+            DamageIndicator.Initialize(GetComboDamage);
+
+            DianaMenu = MainMenu.AddMenu("VnHarry Diana", "diana");
+            DianaMenu.AddGroupLabel("Diana");
+            DianaMenu.AddSeparator();
+            DianaMenu.AddLabel("VnHarry Diana V1.0.0.0");
+            //combo
+            ComboMenu = DianaMenu.AddSubMenu("Combo", "combo");   
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.Add("combo.q", new CheckBox("Use Q", true));
+            ComboMenu.Add("combo.w", new CheckBox("Use W", true));
+            ComboMenu.Add("combo.e", new CheckBox("Use E", true));
+            ComboMenu.Add("combo.r", new CheckBox("Use R", true));
+            ComboMenu.Add("combo.secure", new CheckBox("Use R to secure kill", true));
+            ComboMenu.AddGroupLabel("R Settings");
+            ComboMenu.Add("combo.mod", new Slider("Normal (Q->R) | Misaya Combo (R->Q)", 0, 0, 1));
+            ComboMenu.Add("combo.misayaminrange", new Slider("R Minimum Range for Misaya", (int)(R.Range * 0.8), 0, (int)R.Range));
+            ComboMenu.Add("combo.preventundertower", new Slider("Don't use ult if HP% <", 20, 0, 100));
+            ComboMenu.Add("combo.UseSecondRLimitation", new Slider("Max close enemies for secure kill with R", 5, 1, 5));
+
+            //Harass Menu
+            HarassMenu = DianaMenu.AddSubMenu("HarassMenu", "harass");
+            HarassMenu.Add("harass.q", new CheckBox("Use Q", true));
+            HarassMenu.Add("harass.w", new CheckBox("Use W", true));
+            HarassMenu.Add("harass.e", new CheckBox("Use E", true));
+            HarassMenu.Add("harass.mana", new Slider("Mana manager (%)", 55, 0, 100));
+            //LaneClear Menu
+            LaneClearMenu = DianaMenu.AddSubMenu("LaneClear Settings", "laneclear");
+            LaneClearMenu.Add("laneclear.q", new CheckBox("Use Q", true));
+            LaneClearMenu.Add("laneclear.qcount", new Slider("Minions in range for Q", 2, 1, 5));
+            LaneClearMenu.Add("laneclear.w", new CheckBox("Use W", true));
+            LaneClearMenu.Add("laneclear.wcount", new Slider("Minions in range for W", 2, 1, 5));
+            LaneClearMenu.Add("laneclear.e", new CheckBox("Use E", true));
+            LaneClearMenu.Add("laneclear.ecount", new Slider("Minions in range for E", 2, 1, 5));
+            LaneClearMenu.Add("laneclear.r", new CheckBox("Use E", false));
+
+            LaneClearMenu.Add("laneclear.mana", new Slider("Mana manager (%)", 50, 0, 100));
+            //DrawingsMenu
+            DrawingsMenu = DianaMenu.AddSubMenu("Drawings Settings", "drawingsmenu");
+            DrawingsMenu.AddGroupLabel("Drawings Settings");
+            DrawingsMenu.Add("drawing.q", new CheckBox("Draw Q"));
+            DrawingsMenu.Add("drawing.w", new CheckBox("Draw W"));
+            DrawingsMenu.Add("drawing.e", new CheckBox("Draw E"));
+            DrawingsMenu.Add("drawing.r", new CheckBox("Draw R"));
+
+            Game.OnTick += Game_OnTick;
+            Drawing.OnDraw += Drawing_OnDraw;
+          
+        }
+
+        public static float GetComboDamage(Obj_AI_Base enemy)
+        {
+            float damage = 0;
+
+            if (Q.IsReady())
+            {
+                damage += QDamage(enemy);
+            }
+
+            if (W.IsReady())
+            {
+                damage += WDamage(enemy);
+            }
+
+
+            if (R.IsReady())
+            {
+                damage += RDamage(enemy);
+            }
+
+            return damage;
+        }
+        public static float QDamage(Obj_AI_Base target)
+        {
+            return Program._Player.CalculateDamageOnUnit(target, DamageType.Magical,
+                (float)(new[] { 60, 95, 130, 165, 200 }[Q.Level] + 0.7 * _Player.FlatMagicDamageMod));
+        }
+        public static float WDamage(Obj_AI_Base target)
+        {
+            return Program._Player.CalculateDamageOnUnit(target, DamageType.Magical,
+                (float)(new[] { 66, 102, 138, 174, 210 * 3 }[W.Level] + 0.6 * _Player.FlatMagicDamageMod));
+        }
+        public static float RDamage(Obj_AI_Base target)
+        {
+            return Program._Player.CalculateDamageOnUnit(target, DamageType.Magical,
+                (float)(new[] { 100, 160, 220 }[R.Level] + 0.6 * _Player.FlatMagicDamageMod));
+        }
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (!_Player.IsDead)
+            {
+                var drawQ = DrawingsMenu["drawing.q"].Cast<CheckBox>().CurrentValue;
+                var drawW = DrawingsMenu["drawing.w"].Cast<CheckBox>().CurrentValue;
+                var drawE = DrawingsMenu["drawing.e"].Cast<CheckBox>().CurrentValue;
+                var drawR = DrawingsMenu["drawing.r"].Cast<CheckBox>().CurrentValue;              
+
+                if (drawQ && Q.Level > 0 && Q.IsReady())
+                {
+                    new Circle { Color = System.Drawing.Color.BlueViolet, BorderWidth = 1, Radius = Q.Range }.Draw(Player.Instance.Position);
+                }
+                if (drawW && W.Level > 0 && W.IsReady())
+                {
+                    new Circle { Color = System.Drawing.Color.BlueViolet, BorderWidth = 1, Radius = W.Range }.Draw(Player.Instance.Position);
+                }
+                if (drawE && E.Level > 0 && E.IsReady())
+                {
+                    new Circle { Color = System.Drawing.Color.BlueViolet, BorderWidth = 1, Radius = E.Range }.Draw(Player.Instance.Position);
+                }
+                if (drawR && R.Level > 0 && R.IsReady())
+                {
+                    new Circle { Color = System.Drawing.Color.BlueViolet, BorderWidth = 1, Radius = R.Range }.Draw(Player.Instance.Position);
+                }
+
+                if (true)
+                {
+                    var minionList = EntityManager.GetLaneMinions(EntityManager.UnitTeam.Enemy, Program._Player.ServerPosition.To2D(), Program._Player.AttackRange + 500);
+                    foreach (Obj_AI_Minion minion in minionList)
+                    {
+                        {
+                            if (minion.Health <= Program._Player.GetAutoAttackDamage(minion, true))
+                                new Circle { Color = System.Drawing.Color.Lime, BorderWidth = 1, Radius = minion.BoundingRadius }.Draw(minion.Position);
+                            else if (minion.Health <= Program._Player.GetAutoAttackDamage(minion, true) * 2)
+                                new Circle { Color = System.Drawing.Color.Gold, BorderWidth = 1, Radius = minion.BoundingRadius }.Draw(minion.Position);
+                        }
+                    }
+                }
+
+                if (true)
+                {
+                    var target = Orbwalker.GetTarget();
+
+                    if (target != null)
+                        new Circle { Color = System.Drawing.Color.Red, BorderWidth = 1, Radius = target.BoundingRadius + 15 }.Draw(target.Position);
+                }
+            }
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo))
+            {
+                var ultType = ComboMenu["combo.mod"].Cast<Slider>().CurrentValue;
+
+                
+                if (ultType == 0)
+                {
+                    StateHandler.Combo();
+                }
+                else if (ultType == 1)
+                {
+                    StateHandler.MisayaCombo();
+                }
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Harass))
+            {
+                StateHandler.Harass();
+            }
+            else if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.LaneClear))
+            {
+                StateHandler.WaveClear();
+            }
+           }
+    }
+}
---
+++ Diana/StateHandler.cs
@@ -0,0 +1,291 @@
+﻿using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+using System.Linq;
+
+namespace VnHarry_Diana
+{
+    internal class StateHandler
+    {
+        public static bool IsUnderTurret(Vector3 position)
+        {
+            return ObjectManager.Get<Obj_AI_Turret>().Any(turret => turret.IsValidTarget(950) && turret.IsEnemy);
+        }
+
+        public static void Combo()
+        {
+            var target = TargetSelector2.GetTarget(Program.Q.Range, DamageType.Magical);
+            if (target == null || !target.IsValid)
+            {
+                return;
+            }
+            var useQ = Program.ComboMenu["combo.q"].Cast<CheckBox>().CurrentValue;
+            var useW = Program.ComboMenu["combo.w"].Cast<CheckBox>().CurrentValue;
+            var useE = Program.ComboMenu["combo.e"].Cast<CheckBox>().CurrentValue;
+            var useR = Program.ComboMenu["combo.r"].Cast<CheckBox>().CurrentValue;
+            var secondR = Program.ComboMenu["combo.secure"].Cast<CheckBox>().CurrentValue;
+            var UseSecondRLimitation = Program.ComboMenu["combo.UseSecondRLimitation"].Cast<Slider>().CurrentValue;
+            var minHpToDive = Program.ComboMenu["combo.preventundertower"].Cast<Slider>().CurrentValue;
+
+            if (useQ && Program.Q.IsReady() && Program.Q.IsInRange(target))
+            {
+                var pred = Program.Q.GetPrediction(target);
+                if (pred.HitChance >= EloBuddy.SDK.Enumerations.HitChance.Medium)
+                {
+                    Program.Q.Cast(target);
+                }
+            }
+
+            if (useR && Program.R.IsReady() && Program.R.IsInRange(target)
+                && target.HasBuff("dianamoonlight")
+                 && (!IsUnderTurret(target.ServerPosition) || (minHpToDive <= Program._Player.HealthPercent)))
+            {
+                Program.R.Cast(target);
+            }
+
+            if (useW && Program.W.IsReady() && Program.W.IsInRange(target))
+            {
+                Program.W.Cast();
+            }
+
+            if (useE && Program.E.IsReady() && Program.E.IsInRange(target))
+            {
+                Program.E.Cast();
+            }
+
+            if (secondR && (!IsUnderTurret(target.ServerPosition) || (minHpToDive <= Program._Player.HealthPercent)))
+            {
+                var closeEnemies = HeroManager.Enemies.Where<AIHeroClient>(a => a.Distance(Program._Player) < Program.R.Range * 2 && !a.IsInvulnerable && !a.IsZombie && !a.IsDead);
+                int enemycount = 0;
+                foreach (AIHeroClient enemy in closeEnemies)
+                {
+                    enemycount++;
+                }
+                if (enemycount <= UseSecondRLimitation && useR && !Program.Q.IsReady()
+                && Program.R.IsReady())
+                {
+                    if (target.Health < Program.RDamage(target)
+                        && (!IsUnderTurret(target.ServerPosition) || (minHpToDive <= Program._Player.HealthPercent)))
+                    {
+                        Program.R.Cast(target);
+                    }
+                }
+
+                if (enemycount <= UseSecondRLimitation && Program.R.IsReady())
+                {
+                    if (target.Health < Program.RDamage(target))
+                    {
+                        Program.R.Cast(target);
+                    }
+                }
+            }
+
+            //Ignite
+            if (Program.gotIgnite && target != null && Program.ignite != SpellSlot.Unknown &&
+                Program._Player.Spellbook.CanUseSpell(Program.ignite) == SpellState.Ready & !target.IsInvulnerable & !target.IsZombie)
+            {
+                if (ObjectManager.Player.GetSummonerSpellDamage(target, DamageLibrary.SummonerSpells.Ignite) > target.Health)
+                {
+                    Program._Player.Spellbook.CastSpell(Program.ignite, target);
+                    Program.R.Cast(target);
+                    return;
+                }
+            }
+        }
+        public static void MisayaCombo()
+        {
+            var target = TargetSelector.GetTarget(Program.Q.Range, DamageType.Magical);
+            if (target == null || !target.IsValid)
+            {
+                return;
+            }
+
+            var useQ = Program.ComboMenu["combo.q"].Cast<CheckBox>().CurrentValue;
+            var useW = Program.ComboMenu["combo.w"].Cast<CheckBox>().CurrentValue;
+            var useE = Program.ComboMenu["combo.e"].Cast<CheckBox>().CurrentValue;
+            var useR = Program.ComboMenu["combo.r"].Cast<CheckBox>().CurrentValue;
+            var secondR = Program.ComboMenu["combo.secure"].Cast<CheckBox>().CurrentValue;
+            var UseSecondRLimitation = Program.ComboMenu["combo.UseSecondRLimitation"].Cast<Slider>().CurrentValue;
+            var minHpToDive = Program.ComboMenu["combo.preventundertower"].Cast<Slider>().CurrentValue;
+            var misayaMinRange = Program.ComboMenu["combo.misayaminrange"].Cast<Slider>().CurrentValue;
+            var distToTarget = Program._Player.Distance(target, false);
+           
+            // Can use R, R is ready but player too far from the target => do nothing
+            if (useR && Program.R.IsReady() && distToTarget > Program.R.Range)
+            {
+                return;
+            }
+
+            // Prerequisites for Misaya Combo : If target is too close, won't work
+            if (useQ && useR && Program.Q.IsReady() && Program.R.IsReady()
+                && distToTarget >= misayaMinRange)
+            {
+                Program.R.Cast(target);
+                // No need to check the hitchance since R is a targeted dash.
+                Program.Q.Cast(target);
+            }
+
+            // Misaya Combo is not possible, classic mode then
+
+            if (useQ && Program.Q.IsReady() && Program.Q.IsInRange(target))
+            {
+              
+                    Program.Q.Cast(target);
+            }
+
+            if (useR && Program.R.IsReady() && Program.R.IsInRange(target)
+                && target.HasBuff("dianamoonlight"))
+            {
+                Program.R.Cast(target);
+            }
+
+            if (useW && Program.W.IsReady() && Program.W.IsInRange(target))
+            {
+                Program.W.Cast();
+            }
+
+            if (useE && Program.E.IsReady() && Program.E.IsInRange(target))
+            {
+
+                Program.E.Cast();
+
+            }
+
+            if (secondR)
+            {
+                var closeEnemies = HeroManager.Enemies.Where<AIHeroClient>(a => a.Distance(Program._Player) < Program.R.Range * 2 && !a.IsInvulnerable && !a.IsZombie && !a.IsDead);
+                int enemycount = 0;
+                foreach (AIHeroClient enemy in closeEnemies)
+                {
+                    enemycount++;
+                }
+                if (enemycount <= UseSecondRLimitation && useR && !Program.Q.IsReady()
+                    && Program.R.IsReady())
+                {
+                    if (target.Health < Program.RDamage(target))
+                    {
+                        Program.R.Cast(target);
+                    }
+                }
+
+                if (enemycount <= UseSecondRLimitation && Program.R.IsReady())
+                {
+                    if (target.Health < Program.RDamage(target))
+                    {
+                        Program.R.Cast(target);
+                    }
+                }
+            }
+
+            //Ignite
+            if (Program.gotIgnite && target != null && Program.ignite != SpellSlot.Unknown &&
+                Program._Player.Spellbook.CanUseSpell(Program.ignite) == SpellState.Ready & !target.IsInvulnerable & !target.IsZombie)
+            {
+                if (ObjectManager.Player.GetSummonerSpellDamage(target, DamageLibrary.SummonerSpells.Ignite) > target.Health)
+                {
+                    Program._Player.Spellbook.CastSpell(Program.ignite, target);
+                    Program.R.Cast(target);
+                    return;
+                }
+            }
+        }
+
+        public static void Harass()
+        {
+            var target = TargetSelector2.GetTarget(Program.Q.Range, DamageType.Physical);
+            if (target == null || !target.IsValid)
+            {
+                return;
+            }
+
+            var useQ = Program.HarassMenu["harass.q"].Cast<CheckBox>().CurrentValue;
+            var useW = Program.HarassMenu["harass.w"].Cast<CheckBox>().CurrentValue;
+            var useE = Program.HarassMenu["harass.e"].Cast<CheckBox>().CurrentValue;
+            var checkMana = Program.ComboMenu["harass.mana"].Cast<Slider>().CurrentValue;
+
+            if (Program._Player.ManaPercent < checkMana)
+            {
+                return;
+            }
+
+            if (useQ && Program.Q.IsReady() && Program.Q.IsInRange(target))
+            {
+                var pred = Program.Q.GetPrediction(target);
+                Program.Q.Cast(target);
+            }
+
+            if (useW && Program.W.IsReady() && Program.W.IsInRange(target))
+            {
+                Program.W.Cast();
+            }
+
+            if (useE && Program.E.IsReady() && Program.E.IsInRange(target))
+            {
+                Program.E.Cast();
+            }
+        }
+
+       
+
+        public static void WaveClear()
+        {
+            var minion = EntityManager.GetLaneMinions(EntityManager.UnitTeam.Enemy, Program._Player.ServerPosition.To2D(), Program.Q.Range).FirstOrDefault();
+            if (minion == null)
+            {
+                return;
+            }
+
+            var useQ = Program.LaneClearMenu["laneclear.q"].Cast<CheckBox>().CurrentValue;
+            var useW = Program.LaneClearMenu["laneclear.w"].Cast<CheckBox>().CurrentValue;
+            var useE = Program.LaneClearMenu["laneclear.e"].Cast<CheckBox>().CurrentValue;
+            var useR = Program.LaneClearMenu["laneclear.r"].Cast<CheckBox>().CurrentValue;
+
+            var countQ = Program.LaneClearMenu["laneclear.qcount"].Cast<Slider>().CurrentValue;
+            var countW = Program.LaneClearMenu["laneclear.wcount"].Cast<Slider>().CurrentValue;
+            var countE = Program.LaneClearMenu["laneclear.ecount"].Cast<Slider>().CurrentValue;
+
+            var minions = EntityManager.GetLaneMinions(EntityManager.UnitTeam.Enemy, Program._Player.ServerPosition.To2D(), Program.Q.Range);
+
+            var qMinions = minions.FindAll(minionQ => minion.IsValidTarget(Program.Q.Range));
+            var qMinion = qMinions.Find(minionQ => minionQ.IsValidTarget());
+
+            if (useQ && Program.Q.IsReady())
+            // && Program._Player. >= countQ)
+            {
+                Program.Q.Cast(qMinion);
+            }
+
+            if (useW && Program.W.IsReady())
+            //  && spells[Spells.W].GetCircularFarmLocation(minions).MinionsHit >= countW)
+            {
+                Program.W.Cast();
+            }
+
+            if (useE && Program.E.IsReady() && Program._Player.Distance(qMinion, false) < 200)
+            //  && spells[Spells.E].GetCircularFarmLocation(minions).MinionsHit >= countE)
+            {
+                Program.E.Cast();
+            }
+
+            var minionsR = EntityManager.GetLaneMinions(EntityManager.UnitTeam.Enemy, Program._Player.ServerPosition.To2D(), Program.Q.Range);
+
+            if (useR && Program.R.IsReady())
+            {
+                foreach (var canBeKilled in minionsR)
+                {
+                    if (canBeKilled.HasBuff("dianamoonlight"))
+                    {
+                        if (canBeKilled.Health < Program.RDamage(minion))
+                        {
+                            if (canBeKilled.IsValidTarget())
+                            {
+                                Program.R.Cast(canBeKilled);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
